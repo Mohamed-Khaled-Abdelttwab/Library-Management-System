8b8cfaaae303e30db08d71ed93e8f717ed57a7c3
Mohamed Khaled <Mohamed.Khaled@gmail.com>
2024-05-01 20:09:46 +0300

    Add project files.

---
+++ Library_System/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_System
+{
+    internal class Library
+    {
+        private List<Book> books;
+        private List<Book> BorrowedBooks;
+
+        public void AddBook()
+        {
+            // add logic
+        }
+        public void RemoveBook()
+        {
+            // add logic
+        }
+        public void Display()
+        {
+            // add logic
+        }
+    }
+}
